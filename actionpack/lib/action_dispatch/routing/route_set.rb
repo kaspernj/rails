8956e4ca4a5b7cd0c7abba95332b976f52106702
kaspernj <kaspernj@gmail.com>
2021-01-11 18:01:39 +0100

    Changed error to warn

--- actionpack/lib/action_dispatch/routing/route_set.rb
+++ actionpack/lib/action_dispatch/routing/route_set.rb
@@ -575,7 +575,7 @@ module ActionDispatch
         raise ArgumentError, "Invalid route name: '#{name}'" unless name.blank? || name.to_s.match(/^[_a-z]\w*$/i)
 
         if name && named_routes[name]
-          raise ArgumentError, "Invalid route name, already in use: '#{name}' \n" \
+          Rails.logger.warn "Invalid route name, already in use: '#{name}' \n" \
             "You may have defined two routes with the same name using the `:as` option, or " \
             "you may be overriding a route already defined by a resource with the same naming. " \
             "For the latter, you can restrict the routes created with `resources` as explained here: \n" \
